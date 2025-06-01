38aa1c84335ad8cc841dc4c4550f19c0fb9badfd
oliolga <den-olia@hotmail.it>
2025-06-01 21:02:47 +0200

    Create Status.php

---
+++ Status.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <title>Обработка платежа</title>
+</head>
+<body>
+    <h1>Спасибо!</h1>
+    <p>Ваш платёж обрабатывается. Мы свяжемся с вами, если потребуется.</p>
+</body>
+</html>
